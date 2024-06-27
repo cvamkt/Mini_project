563e534befd3ac18f83f069ec19d5f7e6706c149
shivam kumar <shivamkrthakur9771@gmail.com>
2024-06-27 16:42:39 +0530

    restored

--- CLOCK/time.js
+++ CLOCK/time.js
@@ -13,15 +13,4 @@ setInterval(function () {
 
 
 
-const clock = document.getElementById('clock')
-
-
-
-
-
-setInterval(function () {
-    let date = new Date();
-    // console.log(date.toLocaleTimeString());
-    clock.innerHTML = `<span>${date.toLocaleTimeString()}</span>`;
-}, 1000);
 
