fc819af475df270a9b60b168beb2fd39ca3d01ce
shivam kumar <shivamkrthakur9771@gmail.com>
2024-06-27 16:35:58 +0530

    new code added

--- CLOCK/time.js
+++ CLOCK/time.js
@@ -10,3 +10,18 @@ setInterval(function () {
     clock.innerHTML = `<span>${date.toLocaleTimeString()}</span>`;
 }, 1000);
 
+
+
+
+const clock = document.getElementById('clock')
+
+
+
+
+
+setInterval(function () {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = `<span>${date.toLocaleTimeString()}</span>`;
+}, 1000);
+
